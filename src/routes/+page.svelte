ce3b346089210765f145696cb8af64cf1ae54d75
tonytomo <fathonisatrioutomo@gmail.com>
2025-12-11 15:01:14 +0700

    feat: init project

---
+++ src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	const title = 'This is a real life';
+</script>
+
+<svelte:head>
+	<title>{title}</title>
+</svelte:head>
+
+<h1>Todos:</h1>
+<ul>
+	<li>Init project</li>
+	<li></li>
+	<li></li>
+	<li></li>
+	<li></li>
+	<li></li>
+	<li></li>
+	<li></li>
+	<li></li>
+	<li></li>
+</ul>
